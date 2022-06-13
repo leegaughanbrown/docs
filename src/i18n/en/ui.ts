68849a791b07e6b41c96fe5998b37741c2656733
cbontems <67456035+cbontems@users.noreply.github.com>
2022-06-13 14:41:23 +0200

    Site title and description

--- src/i18n/en/ui.ts
+++ src/i18n/en/ui.ts
@@ -2,8 +2,8 @@ export default {
 	'a11y.skipLink': 'Skip to Content',
 	'navbar.a11yTitle': 'Top Navigation',
 	// Site settings
-	'site.title': 'Astro Documentation',
-	'site.description': 'Build faster websites with less client-side Javascript.',
+	'site.title': 'OxyProps Documentation',
+	'site.description': 'Bring the power of CSS Custom Properties to Oxygen Buidler.',
 	'site.og.imageSrc': '/default-og-image.png?v=1',
 	'site.og.imageAlt': 'astro logo on a starry expanse of space, with a purple saturn-like planet floating in the right foreground',
 	// Left Sidebar
