1cd8b258b92a65ecf6d1d1ed6085804e44519337
cbontems <67456035+cbontems@users.noreply.github.com>
2022-06-13 14:44:03 +0200

    Update languages.ts

--- src/i18n/languages.ts
+++ src/i18n/languages.ts
@@ -4,22 +4,5 @@
  */
 export default {
 	en: 'English',
-	de: 'Deutsch',
-	nl: 'Nederlands',
-	'pt-BR': 'Português do Brasil',
-	fi: 'Suomi',
-	es: 'Español',
-	'zh-CN': '简体中文',
-	'zh-TW': '正體中文',
-	bg: 'Български',
 	fr: 'Français',
-	bn: 'বাংলা',
-	kr: '한국어',
-	ar: 'العربية',
-	da: 'Dansk',
-	ja: '日本語',
-	ru: 'Русский',
-	it: 'Italiano',
-	pl: 'Polish',
-	hu: 'Hungarian',
 };
